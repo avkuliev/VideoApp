61b3124bf4f324f2fd0242070552ab556b0f5ab2
Alexander Kuliev <avkuliev@gmail.com>
2014-10-04 18:40:08 +0400

    Initial Commit

---
+++ VideoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VideoApp
+//
+//  Created by Alexander Kuliev on 10/4/14.
+//  Copyright (c) 2014 Alexander Kuliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ VideoAppTests/VideoAppTests.m
@@ -0,0 +1,34 @@
+//
+//  VideoAppTests.m
+//  VideoAppTests
+//
+//  Created by Alexander Kuliev on 10/4/14.
+//  Copyright (c) 2014 Alexander Kuliev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VideoAppTests : XCTestCase
+
+@end
+
+@implementation VideoAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
